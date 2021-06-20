fa1e843f917d3d98fcf23b3a5bb814d0cc58f8cb
Titas <tlasic16@student.aau.dk>
2021-06-20 21:28:25 +0200

    updates

--- VCF/Source/PluginEditor.cpp
+++ VCF/Source/PluginEditor.cpp
@@ -17,10 +17,17 @@ VCFAudioProcessorEditor::VCFAudioProcessorEditor (VCFAudioProcessor& p, juce::Au
     // editor's size to whatever you need it to be.
     setSize (300, 250);
     
+
+    controlK.setColour(0x1001400, juce::Colour::fromRGBA(0x80, 0x80, 0x80, 0x80));
+    controlF0.setColour(0x1001400, juce::Colour::fromRGBA(0x20, 0x20, 0x20, 0x80));
+    controlF0.setColour(0x1001700, juce::Colour::fromRGBA(0x00, 0x00, 0x00, 0x00));
+    controlVt.setColour(0x1001400, juce::Colour::fromRGBA(0x80, 0x80, 0x80, 0x80));
+    
+
     // these define the parameters of our slider object
     controlK.setSliderStyle (juce::Slider::LinearBarVertical);
     controlK.addListener(this);
-    controlK.setTextBoxStyle (juce::Slider::NoTextBox, false, 90, 0);
+    controlK.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 60, 20);
     addAndMakeVisible(controlK);
     sliderAttachK.reset(new juce::AudioProcessorValueTreeState::SliderAttachment(audioTree, "controlK_ID", controlK));
     controlK.setPopupDisplayEnabled (true, false, this);
@@ -31,11 +38,11 @@ VCFAudioProcessorEditor::VCFAudioProcessorEditor (VCFAudioProcessor& p, juce::Au
 
     controlF0.setSliderStyle(juce::Slider::LinearHorizontal);
     controlF0.addListener(this);
-    controlF0.setTextBoxStyle(juce::Slider::NoTextBox, false, 90, 0);
+    controlF0.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 40, 20);
     addAndMakeVisible(controlF0);
     sliderAttachF0.reset(new juce::AudioProcessorValueTreeState::SliderAttachment(audioTree, "controlF0_ID", controlF0));
     controlF0.setPopupDisplayEnabled(true, false, this);
-    labelF0.setText(("Fundamental"), juce::dontSendNotification);
+    labelF0.setText(("Cutoff Frequency"), juce::dontSendNotification);
     labelF0.setFont(juce::Font("Slope Opera", 16, 0));
     labelF0.setColour(juce::Label::textColourId, juce::Colour(3, 3, 3));
     addAndMakeVisible(labelF0);
@@ -43,7 +50,7 @@ VCFAudioProcessorEditor::VCFAudioProcessorEditor (VCFAudioProcessor& p, juce::Au
 
     controlVt.setSliderStyle(juce::Slider::LinearBarVertical);
     controlVt.addListener(this);
-    controlVt.setTextBoxStyle(juce::Slider::NoTextBox, false, 90, 0);
+    controlVt.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 60, 20);
     addAndMakeVisible(controlVt);
     sliderAttachVt.reset(new juce::AudioProcessorValueTreeState::SliderAttachment(audioTree, "controlVt_ID", controlVt));
     controlVt.setPopupDisplayEnabled(true, false, this);
@@ -80,15 +87,15 @@ void VCFAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
-    int sliderPaddingBottom = 100, sliderWidth = 40, labelHeight = 40;
+    int sliderPaddingBottom = 100, sliderWidth = 40, labelHeight = 40, fPos = 110;
 
     controlK.setBounds(40, 30, sliderWidth, getHeight() - sliderPaddingBottom);
-    controlF0.setBounds(70, getHeight() - 20, getWidth() - 70, 20);
-    controlVt.setBounds(getWidth() - 80, 30, 20, getHeight() - sliderPaddingBottom);
+    controlF0.setBounds(fPos, getHeight() - 20, getWidth() - fPos, 20);
+    controlVt.setBounds(getWidth() - 80, 30, sliderWidth, getHeight() - sliderPaddingBottom);
 
     labelK.setBounds(40 + sliderWidth / 2 - 80 / 2, getHeight() - labelHeight - 20, 80, labelHeight);
-    labelF0.setBounds(0, getHeight() - 20, 70, 20);
-    labelVt.setBounds((getWidth() - 70 - 80 / 2), getHeight() - labelHeight - 20, 70, labelHeight);
+    labelF0.setBounds(0, getHeight() - 20, fPos, 20);
+    labelVt.setBounds((getWidth() - 70 - 40 / 2), getHeight() - labelHeight - 20, 70, labelHeight);
 
 }
 void VCFAudioProcessorEditor::sliderValueChanged (juce::Slider* slider)
